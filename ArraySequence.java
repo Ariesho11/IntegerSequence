b051b4e538a3c8b87682ae97611d8d68e2389879
Aries <aho11@stuy.edu>
2020-12-07 14:13:30 -0500

    created the first constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,28 @@ public class ArraySequence implements IntegerSequence{
   int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int[] other){
+    data = other;
+    currentIndex = 0;
+  }
+  public ArraySequence(IntegerSequence otherseq){
+
+  }
+  public void reset(){
+    currentIndex = 0;
+    data = null;
+  }
+  public int length(){
+    return 0;
+  }
+  public int next(){
+    if (data.length > currentIndex){
+      currentIndex++;
+      if(currentIndex == 0) return data[currentIndex-1];
+    }
+    return data[currentIndex];
+  }
+  public boolean hasNext(){
+    return(currentIndex < data.length);
+  }
 }
