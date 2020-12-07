552b627f508ffe0bd461116504479af4d7ea6eea
Aries <aho11@stuy.edu>
2020-12-07 11:08:37 -0500

    fixed constructor

--- Range.java
+++ Range.java
@@ -2,6 +2,9 @@ import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start,end,current;
   public Range(int start,  int end){
+    if (start > end){
+      throw new IndexOutOfBoundsException("start is greater than end");
+    }
     this.start = start;
     this.end = end;
     current = start;
@@ -12,9 +15,9 @@ public class Range implements IntegerSequence{
     current = 0;
   }
   public int length(){
-    int diff = Math.abs(end - start);
-    if(diff == 1) return diff+2;
-    if(diff == 0) return 0;
+    int diff = Math.abs(Math.abs(end) - Math.abs(start));
+  //  if(diff == 1) return diff+1;
+    if(diff == 0) return 1;
     return diff+1;
   }
   public boolean hasNext(){
