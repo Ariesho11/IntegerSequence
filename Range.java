7992822e1d57271878fcd97b32bd6a445f23036b
Aries <aho11@stuy.edu>
2020-12-08 21:21:15 -0500

    edited length and hasNext

--- Range.java
+++ Range.java
@@ -15,19 +15,21 @@ public class Range implements IntegerSequence{
     current = 0;
   }
   public int length(){
-    int diff = Math.abs(Math.abs(end) - Math.abs(start));
-  //  if(diff == 1) return diff+1;
+    int diff = 0;
+    if(start < 0)  diff = Math.abs(end + Math.abs(start));
+    if((start < 0)&&(end < 0)) diff = Math.abs(Math.abs(start) - Math.abs(end));
+    else diff = end - start;
     if(diff == 0) return 1;
     return diff+1;
   }
   public boolean hasNext(){
-    return(current != end);
+    return(current <= end);
   }
 
   //@throws NoSuchElementException
   public int next()throws NoSuchElementException{
-   if (hasNext()) current++;
-   return current;
+    current++;
+    return current;
   }
 
 }
